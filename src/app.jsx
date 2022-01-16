d3d094434c22244b8111673cae294cb3b450bcf0
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-01-16 12:07:23 +1100

    setup of context

--- src/app.jsx
+++ src/app.jsx
@@ -1,18 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Context from "./context/context";
 import NewMatch from "./pages/new";
 import Choice from "./pages/choice";
 import Match from "./pages/match";
 
+
 function App() {
+
+  const [context, setContext] = useState({})
+
   return (
-  <BrowserRouter>
-      <Routes>
-          <Route path="/" element={<NewMatch />}/>
-          <Route path="/choice" element={<Choice />}/>
-          <Route path="/match" element={<Match />}/>
-      </Routes>
-    </BrowserRouter>
+    <Context.Provider value={(context, setContext)}>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<NewMatch />}/>
+              <Route path="/choice" element={<Choice />}/>
+              <Route path="/match" element={<Match />}/>
+          </Routes>
+        </BrowserRouter>
+    </Context.Provider>
   )
 }
 
--- src/pages/new.jsx
+++ src/pages/new.jsx
@@ -1,5 +1,39 @@
 const NewMatch = () => {
-    return <h1>New Match</h1>
+
+    const handleSubmit = (event) =>{
+        event.preventDefault()
+        console.log("form submitted")
+        console.log("PlayerA",event.target.playerA.value)
+        console.log("PlayerB",event.target.playerB.value)
+        console.log("sets",event.target.sets.value)
+
+    }
+
+    return (
+        <>
+            <h1>New Match</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <input type="text" placeholder="Player A" name="playerA"/>
+                    <span>vs.</span>
+                    <input type="text" placeholder="Player B" name="playerB"/>
+                </div>
+
+                <div>
+                    <span>Sets</span>
+                    <input type="radio" id="sets1" name="sets" value={1} defaultChecked/>
+                    <label htmlFor="sets1">1</label>
+                    <input type="radio" id="sets3" name="sets" value={3} />
+                    <label htmlFor="sets3">3</label>
+                    <input type="radio" id="sets5" name="sets" value={5} />
+                    <label htmlFor="sets5">5</label>
+                </div>
+                <div>
+                    <button type="submit">Continue</button>
+                </div>
+            </form>
+        </>
+    )
 }
 
 export default NewMatch
